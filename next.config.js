464060a3cd6f92c77f355f1f966c4fa69499d85c
Brian Prado <brravee123@gmail.com>
2023-03-29 10:34:04 +0800

    fix image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withImage=require('next-images')
 const nextConfig = {
 	reactStrictMode: true,
 };
 
 module.exports = nextConfig;
+module.exports = withImage()
