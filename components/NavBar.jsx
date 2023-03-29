ef4ae8c2ca756244cbf218415a177480d90cd0f2
Brian Prado <brravee123@gmail.com>
2023-03-29 10:45:17 +0800

    commit2

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -34,7 +34,7 @@ const NavBar = () => {
 			<div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
 				<Link href="/">
 					<Image
-						src="/../public/assets/logo.png"
+						src="./assets/logo.png"
 						alt="/"
 						width="90"
 						height="50"
