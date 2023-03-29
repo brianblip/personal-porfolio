76d56081504ac3819b264212dee1062487e8d87b
Brian Prado <brravee123@gmail.com>
2023-03-29 10:30:37 +0800

    fixing

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -43,7 +43,9 @@ const NavBar = () => {
 				<div>
 					<ul className="hidden md:flex">
 						<Link href="/">
-							<li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+							<li className="ml-10 text-sm uppercase hover:border-b">
+								Homesss
+							</li>
 						</Link>
 						<Link href="/#about">
 							<li className="ml-10 text-sm uppercase hover:border-b">About</li>
