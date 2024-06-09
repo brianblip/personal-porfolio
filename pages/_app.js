efff0fa7a8520be9ddbb2672c7fe319a2f4e0f88
Brian Prado <pradojohnbrian@gmail.com>
2024-06-09 20:09:40 +0800

    add shadcnUI and assets

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
-import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }) {
-	return (
-		<>
-			<NavBar />
-			<Component {...pageProps} />
-		</>
-	);
+  return (
+    <div className={roboto.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
