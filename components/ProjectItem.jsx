a848a5823730907aa80a5c7c7083fd79b023850c
Brian Prado <pradojohnbrian@gmail.com>
2024-06-09 20:09:54 +0800

    add project

---
+++ components/ProjectItem.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { buttonVariants } from "./ui/button";
+
+const ProjectItem = ({
+  title,
+  backgroundImg,
+  tech,
+  projectUrl,
+  description,
+}) => {
+  return (
+    <div className="flex flex-col rounded-md shadow-lg shadow-gray-300">
+      <Image className="rounded-md" src={backgroundImg} alt="/" />
+
+      <div className="flex-1 pt-6 pl-4 pr-4">
+        <p className="text-3xl tracking-wide ">{title}</p>
+        <p className="pt-1 pl-2 text-lg">{tech}</p>
+        <p className="p-2 text-justify">{description}</p>
+      </div>
+      <div className="flex justify-start mt-4 mb-4 ml-4">
+        <Link
+          className={buttonVariants({ variant: "default" })}
+          href={projectUrl}
+        >
+          View Projects
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,86 +1,132 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-
-const Project = ({ imageSrc, title, description, link }) => {
-	const [showFull, setShowFull] = useState(false);
-
-	const getDescription = (text, maxLength) => {
-		return showFull ? text : text.slice(0, maxLength) + "...";
-	};
-
-	const showReadMore = (text, maxLength) => {
-		return text.length > maxLength && !showFull;
-	};
-
-	const toggleShowFull = () => {
-		setShowFull(!showFull);
-	};
-
-	return (
-		<div className="shadow-xl shadow-gray-300 rounded-xl lg:p-4">
-			<Image
-				className="rounded-sm hover:scale-105 ease-in duration-300 w-full h-[170px] object-cover"
-				src={imageSrc}
-				width="800"
-				height="500"
-				alt="/"
-			/>
-			<div>
-				<h5 className="text-l tracking-wider py-3">{title}</h5>
-				<p className="pb-6 pt-2">
-					{getDescription(description, 140)}
-					{showReadMore(description, 140) && (
-						<span
-							className="text-center text-lg text-blue-500 cursor-pointer"
-							onClick={toggleShowFull}
-						>
-							Read More
-						</span>
-					)}
-				</p>
-
-				<Link href={link}>
-					<h4 className="text-center py-2 rounded-lg bg-[#e7f4ff] px-5 text-lg text-[#19A7CE]">
-						View Project
-					</h4>
-				</Link>
-			</div>
-		</div>
-	);
-};
+import ProjectItem from "./ProjectItem";
+import calcu from "../public/assets/projects/calcu.png";
+import documentTracking from "../public/assets/projects/document-tracking.png";
+import votingApp from "../public/assets/projects/voting-app.png";
+import swiftForm from "../public/assets/projects/swiftform.png";
+import budgetTracker from "../public/assets/projects/budget-tracker.png";
 
 const Projects = () => {
-	return (
-		<div id="projects" className="w-full lg:h-screen py-10">
-			<div className="max-w-[1240px] m-auto px-2 py-16 w-full">
-				<p className="text-xl tacking-widest uppercase text-[#146C94]">
-					Projects
-				</p>
-				<h2 className="py-4">What I&apos;ve Built</h2>
-				<div className="grid md:grid-cols-3 gap-8">
-					<Project
-						imageSrc="/assets/projects/calcu.png"
-						title="Calc"
-						description="Calcu is an online calculator that allows users to perform basic arithmetic operations. Built using HTML,CSS and Javascript, the application provides a simple and intuitive user interface that allows users to perform calculations with ease."
-						link="https://calcu-one.vercel.app/"
-					/>
-					<Project
-						imageSrc="/assets/projects/document-tracking.png"
-						title="Document Request Tracking System"
-						description="The Document Request Tracking System is a web-based application designed to streamline the process of managing and tracking document requests. Built using JavaScript and Firebase, the system allows users to submit requests for documents, track the status of their requests, and receive notifications when their requests have been fulfilled."
-						link="https://drts.netlify.app/"
-					/>
-					<Project
-						imageSrc="/assets/projects/voting-app.png"
-						title="Online Voting Application"
-						description="The Online Voting Application is a secure and user-friendly web-based platform designed for conducting online voting processes. Built using TypeScript, PHP, and MySQL, the application provides a simple and efficient way for voters to cast their votes remotely, while ensuring the integrity and accuracy of the election results."
-						link="https://online-voting-app-api.000webhostapp.com/#/login"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div id="projects" className="w-full ">
+      <div className="max-w-[1240px] mx-auto px-2 py-16 ">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <p className="py-4">What I&apos;ve Built</p>
+        <div className="grid gap-6 md:grid-cols-2">
+          <ProjectItem
+            title="Document Request Tracking System"
+            backgroundImg={documentTracking}
+            projectUrl="https://drts.netlify.app/"
+            tech="React JS | firebase | Bootstrap | Postman |"
+            description="The Document Request Tracking System is a web app that streamlines document request management. Built with JavaScript and Firebase, it allows users to submit, track, and receive notifications for document requests."
+          />
+          <ProjectItem
+            title="Budget Tracker"
+            backgroundImg={budgetTracker}
+            projectUrl="https://www.budget-tracker.net/"
+            tech="Next JS | Python | PostgresSQL | Tailwindcss | ShadcnUI | Postman |"
+            description="Budget Tracker is a budget app that uses AI to automatically extract item prices from scanned receipts and input them into the website."
+          />
+          <ProjectItem
+            title="Swift Form"
+            backgroundImg={swiftForm}
+            projectUrl="https://swiftform.boomtech.co/login"
+            tech="Next JS | Python | PostgresSQL | Tailwindcss | MantineUI | Figma | Postman |"
+            description="SwiftForm is an AI-powered form builder similar to Google Forms. Users type a prompt, and the AI generates a form based on the prompt."
+          />
+          <ProjectItem
+            title="Online Voting App"
+            backgroundImg={votingApp}
+            projectUrl="https://online-voting-app-api.000webhostapp.com/#/login"
+            tech="React JS | Bootstrap | MySQL | PHP | TypeScript |"
+            description="The Online Voting Application is a secure, user-friendly web platform for conducting online voting. Built with TypeScript, PHP, and MySQL, it enables remote voting while ensuring election integrity."
+          />
+
+          <ProjectItem
+            title="Calculator"
+            backgroundImg={calcu}
+            projectUrl="https://calcu-one.vercel.app/"
+            tech="React JS | CSS | Javascript | "
+            description="Calcu is an online calculator for basic arithmetic. Built with HTML, CSS, and JavaScript, it provides a simple, intuitive interface for easy calculations."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
+
+/* return (
+    <div className="shadow-xl shadow-gray-300 rounded-xl lg:p-4">
+      <Image
+        className="rounded-sm hover:scale-105 ease-in duration-300 w-full h-[170px] object-cover"
+        src={imageSrc}
+        width="800"
+        height="500"
+        alt="/"
+      />
+      <div>
+        <h5 className="py-3 tracking-wider text-l">{title}</h5>
+        <p className="pt-2 pb-6">
+          {getDescription(description, 140)}
+          {showReadMore(description, 140) && (
+            <span
+              className="text-lg text-center text-blue-500 cursor-pointer"
+              onClick={toggleShowFull}
+            >
+              Read More
+            </span>
+          )}
+        </p>
+
+        <Link href={link}>
+          <h4 className="text-center py-2 rounded-lg bg-[#e7f4ff] px-5 text-lg text-[#19A7CE]">
+            View Project
+          </h4>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <div id="projects" className="w-full py-10 lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tacking-widest uppercase text-[#146C94]">
+          Projects
+        </p>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid gap-8 md:grid-cols-3">
+          <Project
+            imageSrc="/assets/projects/calcu.png"
+            title="Calc"
+            description="Calcu is an online calculator that allows users to perform basic arithmetic operations. Built using HTML,CSS and Javascript, the application provides a simple and intuitive user interface that allows users to perform calculations with ease."
+            link="https://calcu-one.vercel.app/"
+          />
+          <Project
+            imageSrc="/assets/projects/document-tracking.png"
+            title="Document Request Tracking System"
+            description="The Document Request Tracking System is a web-based application designed to streamline the process of managing and tracking document requests. Built using JavaScript and Firebase, the system allows users to submit requests for documents, track the status of their requests, and receive notifications when their requests have been fulfilled."
+            link="https://drts.netlify.app/"
+          />
+          <Project
+            imageSrc="/assets/projects/voting-app.png"
+            title="Online Voting Application"
+            description="The Online Voting Application is a secure and user-friendly web-based platform designed for conducting online voting processes. Built using TypeScript, PHP, and MySQL, the application provides a simple and efficient way for voters to cast their votes remotely, while ensuring the integrity and accuracy of the election results."
+            link="https://online-voting-app-api.000webhostapp.com/#/login"
+          />
+          <Project
+            imageSrc="/assets/projects/swiftform.png"
+            title="Online Voting Application"
+            description="The Online Voting Application is a secure and user-friendly web-based platform designed for conducting online voting processes. Built using TypeScript, PHP, and MySQL, the application provides a simple and efficient way for voters to cast their votes remotely, while ensuring the integrity and accuracy of the election results."
+            link="https://online-voting-app-api.000webhostapp.com/#/login"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+ */
